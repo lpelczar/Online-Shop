1c2c4ced5bfdbe3e51d6a8cf2a88120a1076d14a
lpelczar <lukepelczar@gmail.com>
2017-12-21 20:15:51 +0100

    Add fields in product class

---
+++ src/frompythontojava/onlineshop/part1/Product.java
@@ -0,0 +1,14 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.ArrayList;
+
+public class Product {
+
+    private String name;
+    private Float defaultPrice;
+    private ProductCategory productCategory;
+    private static int nextId = 1;
+    private int ID;
+    private static ArrayList<Product> productList;
+
+}
