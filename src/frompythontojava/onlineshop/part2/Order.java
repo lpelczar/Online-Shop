9a0d0572aa5077138a7eff187bf054f637c1f05e
lpelczar <lukepelczar@gmail.com>
2017-12-22 21:28:50 +0100

    Add order class

---
+++ src/frompythontojava/onlineshop/part2/Order.java
@@ -0,0 +1,19 @@
+package frompythontojava.onlineshop.part2;
+
+public class Order implements Orderable {
+
+    private int id;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean checkout() {
+        
+    }
+
+    public boolean pay() {
+
+    }
+}
