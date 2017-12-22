09342de4ef79240563109ee9cf975c10fef9d8c7
lpelczar <lukepelczar@gmail.com>
2017-12-22 21:31:26 +0100

    Add constructor to order class

--- src/frompythontojava/onlineshop/part2/Order.java
+++ src/frompythontojava/onlineshop/part2/Order.java
@@ -3,14 +3,21 @@ package frompythontojava.onlineshop.part2;
 public class Order implements Orderable {
 
     private int id;
+    private static int nextId = 1;
     private String status;
+
+    Order() {
+        this.id = nextId;
+        nextId++;
+        this.status = "new";
+    }
 
     public String getStatus() {
         return status;
     }
 
     public boolean checkout() {
-        
+
     }
 
     public boolean pay() {
