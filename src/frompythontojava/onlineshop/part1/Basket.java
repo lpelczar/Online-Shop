8d702a4066cb1c7d1e07e3ab10f148012892a78a
lpelczar <lukepelczar@gmail.com>
2017-12-22 14:26:35 +0100

    Move Product Iterator to inner class in Basket

--- src/frompythontojava/onlineshop/part1/Basket.java
+++ src/frompythontojava/onlineshop/part1/Basket.java
@@ -4,7 +4,28 @@ import java.util.ArrayList;
 
 public class Basket {
 
-    ArrayList<Product> productList;
-    Iterator iterator = new ProductIterator();
+    private ArrayList<Product> productList;
+    private Iterator iterator = new ProductIterator();
+
+    private class ProductIterator implements Iterator {
+
+        private int index;
+
+        public boolean hasNext() {
+
+            if (index < productList.size()) {
+                return true;
+            }
+            return false;
+        }
+
+        public Object next() {
+
+            if (this.hasNext()) {
+                return productList.get(index++);
+            }
+            return null;
+        }
+    }
 
 }
