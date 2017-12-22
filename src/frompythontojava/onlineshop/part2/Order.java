4a1d49e9ad28f475bdc2d1a09959b919908a9457
lpelczar <lukepelczar@gmail.com>
2017-12-22 21:35:09 +0100

    Implement checkout method

--- src/frompythontojava/onlineshop/part2/Order.java
+++ src/frompythontojava/onlineshop/part2/Order.java
@@ -5,11 +5,14 @@ public class Order implements Orderable {
     private int id;
     private static int nextId = 1;
     private String status;
+    private final String newStatus = "new";
+    private final String checkedStatus = "checked";
+    private final String payedStatus = "payed";
 
     Order() {
         this.id = nextId;
         nextId++;
-        this.status = "new";
+        this.status = newStatus;
     }
 
     public String getStatus() {
@@ -17,7 +20,12 @@ public class Order implements Orderable {
     }
 
     public boolean checkout() {
-
+        if (this.status.equals(newStatus)) {
+            this.status = checkedStatus;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean pay() {
