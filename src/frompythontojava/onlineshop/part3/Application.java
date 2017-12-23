8c2c67ed68316bf0fc43b75ebf9f5cd6eb08b444
lpelczar <lukepelczar@gmail.com>
2017-12-23 16:34:23 +0100

    Add field and constructor to shop controller and start it in app

--- src/frompythontojava/onlineshop/part3/Application.java
+++ src/frompythontojava/onlineshop/part3/Application.java
@@ -3,6 +3,7 @@ package frompythontojava.onlineshop.part3;
 public class Application {
 
     public static void main(String[] args) {
-	// write your code here
+	    ShopController shopController = new ShopController();
+	    shopController.run();
     }
 }
--- src/frompythontojava/onlineshop/part3/ShopController.java
+++ src/frompythontojava/onlineshop/part3/ShopController.java
@@ -1,4 +1,14 @@
 package frompythontojava.onlineshop.part3;
 
+import frompythontojava.onlineshop.part1.Basket;
+
 public class ShopController {
+
+    private Basket basket;
+
+    ShopController() {
+        this.basket = new Basket();
+    }
+
+    public void run(){}
 }
