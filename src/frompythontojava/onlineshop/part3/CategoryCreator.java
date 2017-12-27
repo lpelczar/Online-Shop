c9434f4159b72ed74aee6963d88ab33bd89cbf29
lpelczar <lukepelczar@gmail.com>
2017-12-27 18:11:48 +0100

    Move creating category methods to new category creator class

---
+++ src/frompythontojava/onlineshop/part3/CategoryCreator.java
@@ -0,0 +1,96 @@
+package frompythontojava.onlineshop.part3;
+
+import frompythontojava.onlineshop.part1.*;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class CategoryCreator {
+
+    private ShopView view;
+    private DateValidator dateValidator;
+    private CategoriesContainer categoriesContainer;
+
+    CategoryCreator() {
+        this.view = new ShopView();
+        this.dateValidator = new DateValidator();
+        this.categoriesContainer = new CategoriesContainer();
+    }
+
+    public ArrayList<ProductCategory> getAllCategories() {
+
+        return this.categoriesContainer.getCategories();
+    }
+
+    public void createNewCategory() {
+
+        String name = getCategoryName();
+        boolean isFeaturedCategory = getIsFeaturedCategory();
+        if (!isFeaturedCategory) {
+            if (!this.categoriesContainer.containsProductCategory(name)) {
+                this.categoriesContainer.addCategory(new ProductCategory(name));
+                view.displayCategoryAddedMessage();
+            } else {
+                view.displayCategoryAlreadyExistsMessage();
+            }
+        } else {
+            Date expirationDate = getExpirationDate();
+            if (!this.categoriesContainer.containsFeaturedProductCategory(name, expirationDate)) {
+                this.categoriesContainer.addCategory(new FeaturedProductCategory(name, expirationDate));
+                view.displayCategoryAddedMessage();
+            } else {
+                view.displayCategoryAlreadyExistsMessage();
+            }
+        }
+    }
+
+    private String getCategoryName() {
+
+        int maxNameLength = 15;
+        String name = "";
+        boolean isNameCorrect = false;
+
+        while(!isNameCorrect) {
+            name = view.getCategoryNameInput();
+            if (name.length() < maxNameLength && name.length() > 0) {
+                isNameCorrect = true;
+            }
+        }
+        return name;
+    }
+
+    private boolean getIsFeaturedCategory() {
+
+        final String[] CORRECT_OPTIONS = {"y", "n"};
+        String userInput = "";
+        boolean userInputInCorrectOptions = false;
+
+        while (!userInputInCorrectOptions) {
+            userInput = view.getIsFeaturedCategoryInput();
+            userInputInCorrectOptions = Arrays.asList(CORRECT_OPTIONS).contains(userInput.toLowerCase());
+            if (!userInputInCorrectOptions) {
+                view.displayWrongInputMessage();
+            }
+        }
+        return (userInput.equals(CORRECT_OPTIONS[0]));
+    }
+
+    private Date getExpirationDate() {
+
+        boolean isDateCorrect = false;
+        Date expirationDate = null;
+
+        while(!isDateCorrect) {
+            String dateString = view.getDateInput();
+            try {
+                expirationDate = this.dateValidator.parseDate(dateString, "dd-MM-yyyy");
+                isDateCorrect = true;
+            } catch (ParseException e) {
+                view.displayWrongInputMessage();
+            }
+        }
+        return expirationDate;
+    }
+}
--- src/frompythontojava/onlineshop/part3/ShopController.java
+++ src/frompythontojava/onlineshop/part3/ShopController.java
@@ -2,24 +2,18 @@ package frompythontojava.onlineshop.part3;
 
 import frompythontojava.onlineshop.part1.*;
 
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Date;
-
 public class ShopController {
 
     private Basket basket;
     private ShopView view;
     private Product product;
-    private CategoriesContainer categoriesContainer;
-    private DateValidator dateValidator;
+    private CategoryCreator categoryCreator;
 
 
     ShopController() {
         this.basket = new Basket();
         this.view = new ShopView();
-        this.categoriesContainer = new CategoriesContainer();
-        this.dateValidator = new DateValidator();
+        this.categoryCreator = new CategoryCreator();
     }
 
     public void run(){
@@ -32,91 +26,15 @@ public class ShopController {
             if (userInput.equals("1")) {
                 //createNewProduct();
             } else if (userInput.equals("2")) {
-                createNewCategory();
+                this.categoryCreator.createNewCategory();
             } else if (userInput.equals("7")) {
-                showAllCategories();
+                view.displayAllCategories(this.categoryCreator.getAllCategories());
             } else if (userInput.equals("0")) {
                 shouldExit = true;
             } else {
                 view.displayWrongInputMessage();
             }
         }
-    }
-
-    private void createNewCategory() {
-
-        String name = getCategoryName();
-        boolean isFeaturedCategory = getIsFeaturedCategory();
-        if (!isFeaturedCategory) {
-            if (!this.categoriesContainer.containsProductCategory(name)) {
-                this.categoriesContainer.addCategory(new ProductCategory(name));
-                view.displayCategoryAddedMessage();
-            } else {
-                view.displayCategoryAlreadyExistsMessage();
-            }
-        } else {
-            Date expirationDate = getExpirationDate();
-            if (!this.categoriesContainer.containsFeaturedProductCategory(name, expirationDate)) {
-                this.categoriesContainer.addCategory(new FeaturedProductCategory(name, expirationDate));
-                view.displayCategoryAddedMessage();
-            } else {
-                view.displayCategoryAlreadyExistsMessage();
-            }
-        }
-
-    }
-
-    private String getCategoryName() {
-
-        int maxNameLength = 15;
-        String name = "";
-        boolean isNameCorrect = false;
-
-        while(!isNameCorrect) {
-            name = view.getCategoryNameInput();
-            if (name.length() < maxNameLength && name.length() > 0) {
-                isNameCorrect = true;
-            }
-        }
-        return name;
-    }
-
-    private boolean getIsFeaturedCategory() {
-
-        final String[] CORRECT_OPTIONS = {"y", "n"};
-        String userInput = "";
-        boolean userInputInCorrectOptions = false;
-
-        while (!userInputInCorrectOptions) {
-            userInput = view.getIsFeaturedCategoryInput();
-            userInputInCorrectOptions = Arrays.asList(CORRECT_OPTIONS).contains(userInput.toLowerCase());
-            if (!userInputInCorrectOptions) {
-                view.displayWrongInputMessage();
-            }
-        }
-        return (userInput.equals(CORRECT_OPTIONS[0]));
-    }
-
-    private Date getExpirationDate() {
-
-        boolean isDateCorrect = false;
-        Date expirationDate = null;
-
-        while(!isDateCorrect) {
-            String dateString = view.getDateInput();
-            try {
-                expirationDate = this.dateValidator.parseDate(dateString, "dd-MM-yyyy");
-                isDateCorrect = true;
-            } catch (ParseException e) {
-                view.displayWrongInputMessage();
-            }
-        }
-        return expirationDate;
-    }
-
-    private void showAllCategories() {
-
-        view.displayAllCategories(this.categoriesContainer.getCategories());
     }
 
     private static void clearConsole() {
