7577c5baee288ec66b1ebd6089c0972da6f0acc4
lpelczar <lukepelczar@gmail.com>
2017-12-27 13:10:07 +0100

    Add Categories container class

---
+++ src/frompythontojava/onlineshop/part1/CategoriesContainer.java
@@ -0,0 +1,12 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.ArrayList;
+
+public class CategoriesContainer {
+
+    private ArrayList<ProductCategory> categories;
+
+    CategoriesContainer() {
+        this.categories = new ArrayList<>();
+    }
+}
