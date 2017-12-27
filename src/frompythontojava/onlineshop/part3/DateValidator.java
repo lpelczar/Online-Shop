9700e5e199ecba529c51b5d951153b2443207896
lpelczar <lukepelczar@gmail.com>
2017-12-27 15:14:09 +0100

    Add Date validator class

---
+++ src/frompythontojava/onlineshop/part3/DateValidator.java
@@ -0,0 +1,33 @@
+package frompythontojava.onlineshop.part3;
+
+import jdk.nashorn.internal.runtime.ParserException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateValidator {
+
+    public static Date parseDate(String dateToValidate, String dateFormat) throws ParseException{
+
+        Date date;
+
+        if (dateToValidate == null) {
+            throw new ParseException("Wrong date", 0);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+
+        try {
+
+            //if not valid, it will throw ParseException
+            date = sdf.parse(dateToValidate);
+
+        } catch (ParseException e) {
+
+            throw new ParseException("Wrong date", 0);
+        }
+        return date;
+    }
+}
